317af9fc77c8e073001542d4896570e92c52e945
siddhantkumar20 <siddhant.kumar@ksolves.com>
2024-02-20 17:53:12 +0530

    Assessment 1

---
+++ assessment1/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    //Q1 - JQuery
+    $(".q1 .right").click(function() {
+        $(".q1 .right").css("background-color","rgb(99, 255, 107)");
+    });
+
+    $(".q1 .wrong").click(function() {
+        $(".q1 .right").css("background-color","rgb(99, 255, 107)");
+        $(".q1 .wrong").css("background-color","rgb(255, 140, 140)");
+    });
+
+    //Q2 - JQuery
+    $(".q2 .right").click(function() {
+        $(".q2 .right").css("background-color","rgb(99, 255, 107)");
+    });
+    $(".q2 .wrong").click(function() {
+        $(".q2 .right").css("background-color","rgb(99, 255, 107)");
+        $(".q2 .wrong").css("background-color","rgb(255, 140, 140)");
+    });
+
+    //Q3
+    $(".q3 .right").click(function() {
+        $(".q3 .right").css("background-color","rgb(99, 255, 107)");
+    });
+    $(".q3 .wrong").click(function() {
+        $(".q3 .right").css("background-color","rgb(99, 255, 107)");
+        $(".q3 .wrong").css("background-color","rgb(255, 140, 140)");
+    });
+
+    //Q4
+    $(".q4 .right").click(function() {
+        $(".q4 .right").css("background-color","rgb(99, 255, 107)");
+    });
+    $(".q4 .wrong").click(function() {
+        $(".q4 .right").css("background-color","rgb(99, 255, 107)");
+        $(".q4 .wrong").css("background-color","rgb(255, 140, 140)");
+    });
+
+    //Q5
+    $(".q5 .right").click(function() {
+        $(".q5 .right").css("background-color","rgb(99, 255, 107)");
+    });
+    $(".q5 .wrong").click(function() {
+        $(".q5 .right").css("background-color","rgb(99, 255, 107)");
+        $(".q5 .wrong").css("background-color","rgb(255, 140, 140)");
+    });
+});
+
