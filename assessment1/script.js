2e0ba5d0b7033e294f76efad275f5014674c97cf
siddhantkumar20 <siddhant.kumar@ksolves.com>
2024-02-29 13:31:00 +0530

    Updated

--- assessment1/script.js
+++ assessment1/script.js
@@ -1,48 +1,18 @@
 $(document).ready(function() {
-    //Q1 - JQuery
-    $(".q1 .right").click(function() {
-        $(".q1 .right").css("background-color","rgb(99, 255, 107)");
+
+    var scores = 0;
+
+    $(".q1right, .q2right, .q3right, .q4right, .q5right").click(function() {
+        $(this).css("background-color", "rgb(99, 255, 107)");
+        scores++;
     });
 
-    $(".q1 .wrong").click(function() {
-        $(".q1 .right").css("background-color","rgb(99, 255, 107)");
-        $(".q1 .wrong").css("background-color","rgb(255, 140, 140)");
+    $(".q1wrong, .q2wrong, .q3wrong, .q4wrong, .q5wrong").click(function() {
+        $(this).css("background-color", "rgb(255, 140, 140)");
     });
 
-    //Q2 - JQuery
-    $(".q2 .right").click(function() {
-        $(".q2 .right").css("background-color","rgb(99, 255, 107)");
-    });
-    $(".q2 .wrong").click(function() {
-        $(".q2 .right").css("background-color","rgb(99, 255, 107)");
-        $(".q2 .wrong").css("background-color","rgb(255, 140, 140)");
-    });
-
-    //Q3
-    $(".q3 .right").click(function() {
-        $(".q3 .right").css("background-color","rgb(99, 255, 107)");
-    });
-    $(".q3 .wrong").click(function() {
-        $(".q3 .right").css("background-color","rgb(99, 255, 107)");
-        $(".q3 .wrong").css("background-color","rgb(255, 140, 140)");
-    });
-
-    //Q4
-    $(".q4 .right").click(function() {
-        $(".q4 .right").css("background-color","rgb(99, 255, 107)");
-    });
-    $(".q4 .wrong").click(function() {
-        $(".q4 .right").css("background-color","rgb(99, 255, 107)");
-        $(".q4 .wrong").css("background-color","rgb(255, 140, 140)");
-    });
-
-    //Q5
-    $(".q5 .right").click(function() {
-        $(".q5 .right").css("background-color","rgb(99, 255, 107)");
-    });
-    $(".q5 .wrong").click(function() {
-        $(".q5 .right").css("background-color","rgb(99, 255, 107)");
-        $(".q5 .wrong").css("background-color","rgb(255, 140, 140)");
-    });
+    $('.btn-show-scores').click(function () {
+        alert('Your score: ' + scores);
+    });    
 });
 
