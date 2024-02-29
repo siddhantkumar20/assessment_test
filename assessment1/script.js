79f0ee5d15f9d80678b8223e5b50569f0e3cfe18
siddhantkumar20 <siddhant.kumar@ksolves.com>
2024-02-29 18:14:44 +0530

    Updated

--- assessment1/script.js
+++ assessment1/script.js
@@ -6,6 +6,10 @@ $(document).ready(function() {
         $(this).css("background-color", "rgb(99, 255, 107)");
         scores++;
     });
+
+    $(".q1wrong, .q2wrong, .q3wrong, .q4wrong, .q5wrong").click(function() {
+        $(this).css("background-color", "rgb(199, 0, 0)"); 
+    });
     
     $('.btn-show-scores').click(function () {
         alert('Your score: ' + scores);
